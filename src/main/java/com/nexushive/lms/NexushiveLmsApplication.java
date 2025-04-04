09d971dd48110a37fb3d5385392a47c87d5cdd72
BADRI NARAYANAN <badhrirajen@gmail.com>
2025-04-03 23:03:00 -0400

    Initial Project Structure

---
+++ src/main/java/com/nexushive/lms/NexushiveLmsApplication.java
@@ -0,0 +1,13 @@
+package com.nexushive.lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexushiveLmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexushiveLmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexushive/lms/NexushiveLmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexushive.lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexushiveLmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
